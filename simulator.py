069b819d5dcf6f8c2a308b30bfdbd1ce8dc700cf
Ibrahiem <simplex3@campus.technion.ac.il>
2018-02-08 15:29:57 +0200

    Final commit

--- simulator.py
+++ simulator.py
@@ -1,27 +1,27 @@
 #!python3
+"""Music test (music sample recognition) simulator"""
 
-from moviepy.editor import AudioFileClip as Audio
 from random import randint, sample, randrange
 from os import chdir, path, system as terminal
 from math import floor
 from glob import glob
-from configs import DURATION, DIRECTORY, NUM_OF_CHOICES, NUM_OF_QUESTIONS
 from tempfile import TemporaryDirectory as TempDir
 from time import time
+from moviepy.editor import AudioFileClip as Audio
+from configs import DURATION, DIRECTORY, NUM_OF_CHOICES, NUM_OF_QUESTIONS
 
-
-# TODO: call clipper as a subprocess/thread to suppress verbosity
-# TODO: change to read (single) key
 
 def play(file_name, temp_dir):
+	"""Plays a random audio sample of the file with the provided name"""
 	audio = Audio(file_name)
 	start = randint(0, floor(audio.duration - DURATION))
 	file_path = path.join(temp_dir, str(time()) + '.mp3')
-	audio.subclip(start, start + DURATION).write_audiofile(file_path, verbose=False)
+	audio.subclip(start, start + DURATION).write_audiofile(file_path, verbose=False, progress_bar=False)
 	terminal(file_path)
 
 
 def question(choices, temp_dir):
+	"""Asks a question with multiple answers and returns the correct answer"""
 	answer = randrange(len(choices))
 	play(choices[answer], temp_dir)
 	for i in range(len(choices)):
@@ -31,21 +31,22 @@ def question(choices, temp_dir):
 
 
 def main():
+	"""Run the random music sampler (exam simulator)"""
 	chdir(DIRECTORY)
 	file_names = glob('*.mp3')
 	score = 0
 	with TempDir() as temp_dir:
-		# print('Saving in {}'.format(temp_dir))
 		for i in range(NUM_OF_QUESTIONS):
+			print('Question {}:'.format(i + 1))
 			choices = sample(file_names, NUM_OF_CHOICES)
 			answer = question(choices, temp_dir)
-			key = input()
+			key = input('Answer: ')
 			if key == str(answer):
-				print('[V] Correct!')
+				print('[V] Correct!\n')
 				score = score + 1
 			else:
 				correct = choices[answer]
-				print('[X] Incorrect - {}'.format(correct[:correct.rindex('.')]))
+				print('[X] Incorrect - {}\n'.format(correct[:correct.rindex('.')]))
 	input('Total score {}%'.format(floor((score * 100) / NUM_OF_QUESTIONS)))
 
 
